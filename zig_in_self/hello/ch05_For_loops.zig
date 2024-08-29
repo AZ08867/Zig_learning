7f7262c1661ff0f08c601d79cb0277204bff3a56
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-29 13:16:01 +0800

    add for loop

---
+++ zig_in_self/hello/ch05_For_loops.zig
@@ -0,0 +1,16 @@
+const expect = @import("std").testing.expect;
+
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c', 'd', 'e' };
+    for (string, 0..) |character, index| {
+        _ = character;
+        _ = index;
+    }
+    for (string) |character| {
+        _ = character;
+    }
+    for (string, 0..) |_, index| {
+        _ = index;
+    }
+    for (string) |_| {}
+}
